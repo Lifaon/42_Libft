63b84cfab20a9e659566fd00052e6e77f4022705
Meriadec LANTONNET <mlantonn@student.42.fr>
2018-05-03 15:40:36 +0200

    optimisation ft_atof

--- srcs/ft_atof.c
+++ srcs/ft_atof.c
@@ -6,44 +6,16 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/03 13:57:43 by mlantonn          #+#    #+#             */
-/*   Updated: 2018/05/03 15:08:27 by mlantonn         ###   ########.fr       */
+/*   Updated: 2018/05/03 15:40:13 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static double	get_first_part(char const *str)
-{
-	double	ret;
-	int		i;
-
-	i = 0;
-	ret = 0.0;
-	while (str[i] >= '0' && str[i] <= '9')
-		ret = ret * 10.0 + (str[i++] - '0');
-	return (ret);
-}
-
-static double	get_last_part(char const *str)
+double	ft_atof(char const *str)
 {
 	double	ret;
 	double	coeff;
-	int		i;
-
-	i = 0;
-	ret = 0.0;
-	coeff = 10.0;
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		ret += ((double)(str[i++] - '0') / coeff);
-		coeff *= 10.0;
-	}
-	return (ret);
-}
-
-double			ft_atof(char const *str)
-{
-	double	ret;
 	int		i;
 	int		sign;
 
@@ -53,13 +25,16 @@ double			ft_atof(char const *str)
 	sign = (str[i] == '-' ? -1 : 1);
 	if (str[i] == '-' || str[i] == '+')
 		++i;
-	if (!(str[i] >= '0' && str[i] <= '9') && str[i] != '.')
+	if (!(str[i] >= '0' && str[i] <= '9') && \
+		(str[i] != '.' || !(str[i + 1] >= '0' && str[i + 1] <= '9')))
 		return (0);
-	ret = get_first_part(str + i);
+	ret = 0.0;
 	while (str[i] >= '0' && str[i] <= '9')
-		++i;
-	if (str[i] != '.')
+		ret = ret * 10.0 + (str[i++] - '0');
+	if (str[i++] != '.')
 		return (ret * (double)sign);
-	ret += get_last_part(str + i + 1);
+	coeff = 1.0;
+	while (str[i] >= '0' && str[i] <= '9' && (coeff *= 10.0))
+		ret += ((double)(str[i++] - '0') / coeff);
 	return (ret * (double)sign);
 }
